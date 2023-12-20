eb904f90c65d4cf4c10c0bea61d62f1678fc0870
LachlanRichards375 <lach1an@tpg.com.au>
2023-12-20 10:40:22 +1030

    WIP: Adding C++ DLL Wrapper

---
+++ Assets/Library/WaveFunctionCollapse/dll/WaveFunctionCollapse_CPP.cs
@@ -0,0 +1,103 @@
+
+using System;
+using System.Runtime.InteropServices;
+using FolvosLibrary.WFC;
+
+public class WaveFunctionCollapse_CPP
+{
+	#region C++ Imports
+	[DllImport("WaveFunctionCollapse_CPP.dll")]
+	static extern IntPtr Threaded2DCollapse_Create();
+
+	[DllImport("WaveFunctionCollapse_CPP.dll")]
+	static extern IntPtr Grid2D_Create(IntPtr position);
+
+	[DllImport("WaveFunctionCollapse_CPP.dll")]
+	static extern IntPtr WFCPosition_Create2D(uint x, uint y);
+	[DllImport("WaveFunctionCollapse_CPP.dll")]
+	static extern IntPtr WFCPosition_Create3D(uint x, uint y, uint z);
+	[DllImport("WaveFunctionCollapse_CPP.dll")]
+	static extern IntPtr WFCPosition_Create4D(uint x, uint y, uint z, uint w);
+
+	[DllImport("WaveFunctionCollapse_CPP.dll")]
+	static extern IntPtr IWFCManager_Create(IntPtr collapseMethod, IntPtr grid, short threadCount);
+
+	[DllImport("WaveFunctionCollapse_CPP.dll")]
+	static extern void AddTileToDomain(ulong tilesToAdd);
+
+	[DllImport("WaveFunctionCollapse_CPP.dll")]
+	static extern int IWFCManager_Collapse(IntPtr manager, ulong toCollapseTo, IntPtr position);
+	[DllImport("WaveFunctionCollapse_CPP.dll")]
+	static extern int IWFCManager_Run(IntPtr manager);
+
+	[DllImport("WaveFunctionCollapse_CPP.dll")]
+	static extern void WFCRule_Add_CellIsNot(ulong tile, ulong goal, uint localTargetCount, WFCPosition[] localTargets);
+	#endregion
+
+	public struct CellIsNotRule
+	{
+		public ulong tile;
+		public ulong goal;
+		public WFCPosition[] localTargets;
+	}
+
+	IntPtr manager;
+
+	public WaveFunctionCollapse_CPP Create2DWaveFunctionCollapse(WFCPosition size)
+	{
+		IntPtr collapse = Threaded2DCollapse_Create();
+		IntPtr grid = Grid2D_Create(WFCPositionToIntPtr(size));
+		manager = IWFCManager_Create(collapse, grid, 12);
+		return this;
+	}
+
+	public WaveFunctionCollapse_CPP AddCellIsNotRule(CellIsNotRule ruleToAdd)
+	{
+		WFCRule_Add_CellIsNot(ruleToAdd.tile, ruleToAdd.goal, (uint)ruleToAdd.localTargets.Length, ruleToAdd.localTargets);
+		return this;
+	}
+
+	public WaveFunctionCollapse_CPP AddTilesToDomain(ulong tilesToAdd)
+	{
+		AddTileToDomain(tilesToAdd);
+		return this;
+	}
+
+	public WaveFunctionCollapse_CPP CollapseSpecificCell(ulong toCollapseTo, WFCPosition position)
+	{
+		if (manager == IntPtr.Zero)
+		{
+			//This should throw an error and make me realise what's wrong
+			return null;
+		}
+		IWFCManager_Collapse(manager, toCollapseTo, WFCPositionToIntPtr(position));
+		return this;
+	}
+
+	public WaveFunctionCollapse_CPP RunGenerator()
+	{
+		if (manager == IntPtr.Zero)
+		{
+			//This should throw an error and make me realise what's wrong
+			return null;
+		}
+		IWFCManager_Run(manager);
+		return this;
+	}
+
+	IntPtr WFCPositionToIntPtr(WFCPosition toConvert)
+	{
+		if (toConvert.IsVector4())
+		{
+			return WFCPosition_Create4D((uint)toConvert.x, (uint)toConvert.y, (uint)toConvert.z, (uint)toConvert.w);
+		}
+		else if (toConvert.IsVector3())
+		{
+			return WFCPosition_Create3D((uint)toConvert.x, (uint)toConvert.y, (uint)toConvert.z);
+		}
+		else
+		{
+			return WFCPosition_Create2D((uint)toConvert.x, (uint)toConvert.y);
+		}
+	}
+}
